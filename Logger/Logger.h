fa3fa0d2166aeb41c59071714ab023f1a7de3e3b
Tymianek <70819917+TymianekPL@users.noreply.github.com>
2022-11-12 19:21:20 +0100

    🔒️Add C support

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -1,6 +1,8 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#ifdef __cplusplus
+
 class Logger
 {
 private:
@@ -13,5 +15,14 @@ public:
     void Warn(char* message);
     void Error(char* message);
 };
+
+#else // ! C++
+
+void logger_info(const char* tag, char* message);
+
+void logger_warn(const char* tag, char* message);
+
+void logger_error(const char* tag, char* message);
+#endif // ! C++
 
 #endif
